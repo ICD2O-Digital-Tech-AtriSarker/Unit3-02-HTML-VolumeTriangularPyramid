45d96538aba360f7f8c0e42fcd33e35e3289777d
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-05 15:06:26 +0000

    Main Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+// Script for Volume of a Right-Angled Triangular Pyramid
+
+// Elements
+// Inputs
+const baseInput = document.getElementById('base1Input');
+const base2Input = document.getElementById('base2Input');
+const heightInput = document.getElementById('heightInput');
+// Result Display
+const volumeResult = document.getElementById('volumeResult');
+// Calculate Button
+const calculateBtn = document.getElementById('CalculateBtn');
+
+// Code for calculating volume
+function Calculate() {
+
+  // Get Inputs
+  let a = Number(base1Input.value);
+  let b = Number(base2Input.value);
+  let h = Number(heightInput.value);
+
+  // Calculate Base of the Pyramid
+  let base = a * b * 0.5;
+
+  // Calculate Volume of the Pyramid
+  let volume = base * h * ( 1/3 );
+
+  // Round up to 2 decimal places, if needed
+  if (volume % 1 != 0) {
+    volume = volume.toFixed(2);
+  }
+
+  // Display Result
+  volumeResult.innerHTML = "" + volume + " m<sup>2</sup>";
+  return;
+}
+
+// Connect button click to the Calculate() function, 
+calculateBtn.onclick = Calculate;
+
+// Initial Calculate() call
+Calculate();
